85b415928c19c368c5cb9eed0206b3aeffa4fa89
Estehsan <estehsaan@gmail.com>
2023-04-04 20:23:47 +0200

    Updateio

--- components/HomePage/Testimonials.tsx
+++ components/HomePage/Testimonials.tsx
@@ -11,7 +11,7 @@ const testimonials = [
   [
     {
       content:
-        "MightyHome is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.",
+        "I couldn't be happier with the results of Mighty Home's AI-powered design tool. In just 30 seconds, I was able to see my living room transformed into a modern and inviting space that perfectly suited my style. The ease and convenience of the process were unparalleled.",
       author: {
         name: "Sheryl Berge",
         role: "CEO at Lynch LLC",
@@ -20,9 +20,9 @@ const testimonials = [
     },
     {
       content:
-        "I’m trying to get a hold of someone in support, I’m in a lot of trouble right now and they are saying it has something to do with my books. Please get back to me right away.",
+        "Mighty Home has revolutionized the way I approach interior design! With just a few clicks, I was able to transform my living room into a cozy retreat that perfectly reflects my style. ",
       author: {
-        name: "Amy Hahn",
+        name: "Jane Wilson",
         role: "Director at Velocity Industries",
         image: avatarImage4,
       },
@@ -31,7 +31,7 @@ const testimonials = [
   [
     {
       content:
-        "The best part about TaxPal is every time I pay my employees, my bank balance doesn’t go down like it used to. Looking forward to spending this extra cash when I figure out why my card is being declined.",
+        "I've always struggled with making design decisions for my home. Thanks to Mighty Home, I was able to visualize different options and find the perfect look for my bedroom in no time!",
       author: {
         name: "Leland Kiehn",
         role: "Founder of Kiehn and Sons",
@@ -40,31 +40,11 @@ const testimonials = [
     },
     {
       content:
-        "There are so many things I had to do with my old software that I just don’t do at all with TaxPal. Suspicious but I can’t say I don’t love it.",
+        "I was skeptical at first, but Mighty Home exceeded my expectations. The AI technology is so advanced - it was like the program was reading my mind when it came to my design preferences.",
       author: {
         name: "Erin Powlowski",
         role: "COO at Armstrong Inc",
         image: avatarImage2,
-      },
-    },
-  ],
-  [
-    {
-      content:
-        "I used to have to remit tax to the EU and with TaxPal I somehow don’t have to do that anymore. Nervous to travel there now though.",
-      author: {
-        name: "Peter Renolds",
-        role: "Founder of West Inc",
-        image: avatarImage3,
-      },
-    },
-    {
-      content:
-        "This is the fourth email I’ve sent to your support team. I am literally being held in jail for tax fraud. Please answer your damn emails, this is important.",
-      author: {
-        name: "Amy Hahn",
-        role: "Director at Velocity Industries",
-        image: avatarImage4,
       },
     },
   ],
