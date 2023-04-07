18aaf1c2e929ab45628283e884f5f9e6c5261302
Estehsan <estehsaan@gmail.com>
2023-04-07 02:18:16 +0200

    green color

--- components/Header.tsx
+++ components/Header.tsx
@@ -176,7 +176,7 @@ export default function Header({
               </div>
             ) : (
               <Link
-                className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-400 bg-blue-600 font-medium transition"
+                className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-emerald-600 text-white px-5 py-2 text-sm shadow-md hover:bg-emerald-400 bg-emerald-600 font-medium transition"
                 href="/dream">
                 <p>Sign Up </p>
               </Link>
