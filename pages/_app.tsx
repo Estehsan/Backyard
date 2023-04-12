74dc7cad1d61ec812b3125cd2f0c35422e6be279
estehsan <estehsaan@gmail.com>
2023-04-12 22:05:38 +0200

    basePath updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider basePath="custom-route/api/auth" session={session}>
       <Component {...pageProps} />
       <Analytics />
     </SessionProvider>
